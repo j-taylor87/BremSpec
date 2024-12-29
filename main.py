58b717f6ffd356b654a8930b42116c9af06ca8e4
j-taylor87 <122782276+j-taylor87@users.noreply.github.com>
2024-12-30 09:42:54 +1000

    Update main.py
    Added a CSS workaround to remove whitespace at the top of the app

--- main.py
+++ main.py
@@ -32,6 +32,34 @@ st.set_page_config(layout="wide",
                 #         'About': "!"}
 )
 
+# Current (Oct 24) CSS workaround for edited whitespace of app
+st.markdown("""
+    <style>
+           /* Remove blank space at top and bottom */ 
+           .block-container {
+               padding-top: 1rem;
+               padding-bottom: 1rem;
+            }
+           
+           /* Remove blank space at the center canvas */ 
+           .st-emotion-cache-z5fcl4 {
+               position: relative;
+               top: -62px;
+               }
+           
+           /* Make the toolbar transparent and the content below it clickable */ 
+           .st-emotion-cache-18ni7ap {
+               pointer-events: none;
+               background: rgb(255 255 255 / 0%)
+               }
+           .st-emotion-cache-zq5wmm {
+               pointer-events: auto;
+               background: rgb(255 255 255);
+               border-radius: 5px;
+               }
+    </style>
+    """, unsafe_allow_html=True)
+
 # Main function
 if __name__ == "__main__":
     st.title("BremSpec")
